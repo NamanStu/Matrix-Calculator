df6981385015b5752c3f1d972d863a1857b2c584
Naman Goyal <officialnaman12@gmail.com>
2025-11-13 16:51:08 +0530

    Addition

---
+++ app/MatrixRank/page.js
@@ -0,0 +1,50 @@
+"use client";
+
+export default function Home() {
+
+  return (
+    
+    <main className="max-w-screen grid grid-cols-[55vw_20vw] bg-[#676569]">
+      
+      <div className="mainwindow bg-[#424143] mt-[7vh] ml-2 rounded-2xl">
+
+        <h1 className="text-3xl flex justify-center mt-7 font-bold">Matrix Calculator</h1>
+        <p className="text-[15px] text-[#B5B5B5] m-10 mt-10">Discover our free, advanced matrix calculator. It provides all essential matrix operations and methods for solving systems of simultaneous linear equations, making it a perfect tool for students, engineers, and mathematicians.
+        From basic homework to real-world problems, Reshish matrix calculator has you covered.</p>
+
+        <div className="text-[15px] text-[#B5B5B5] m-10 mt-10">
+
+        <h2 className="text-[17px] font-semibold mt-3">Key Features :</h2>
+
+        <h3 className="ext-[16px] font-semibold underline mt-3">Step-by-step solution:</h3>
+        <p>Our advanced calculator provides a detailed step-by-step solution, making it invaluable for students. For methods requiring intricate calculations it offers a very detailed solution.</p>
+
+        <h3 className="text-[16px] font-semibold underline mt-3">Complex Numbers Support:</h3>
+        <p>Reshish matrix calculator stands out from other tools by supporting complex numbers seamlessly across all operations and methods, making it ideal for advanced calculations.</p>
+
+        <h3 className="text-[16px] font-semibold underline mt-3">Continuous Calculation:</h3>
+        <p>Once you complete an operation, you can immediately perform another using either your original matrix or the calculated result. This eliminates the need to re-enter data and streamlines multi-step calculations.</p>
+
+        </div>
+
+        <div className="text-[15px] text-[#B5B5B5] m-10 mt-10">
+
+        <h2 className="text-[17px] font-semibold mt-3">Why to use a Calculator : </h2>
+
+        <h3 className="ext-[16px] font-semibold underline mt-3">Error Reduction:</h3>
+        <p>Human calculation errors are common in intricate mathematical operations. Our reliable calculator eliminates arithmetic mistakes and ensures consistent results.</p>
+
+        <h3 className="text-[16px] font-semibold underline mt-3">Time Efficiency:</h3>
+        <p>Manual computation can take hours, while our tool delivers results in seconds. This is particularly valuable when working with large matrices.</p>
+
+        <h3 className="text-[16px] font-semibold underline mt-3">Enhanced Learning Experience:</h3>
+        <p>Reshish matrix calculator helps students master matrix operations through detailed step-by-step solutions. This significantly accelerates the learning process.</p>
+
+        </div>
+
+      </div>
+      <div className="ads max-w-[25vw] min-h-screen "></div>
+      
+    </main>
+  );
+}
